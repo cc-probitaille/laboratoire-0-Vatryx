889a76388052b84c93f53ace0f0e85b2a5152600
Gab Lessard <gablessard17@gmail.com>
2025-09-09 16:24:59 -0400

    Étape 4 terminée - Gabriel Lessard

--- src/routes/jeuRouter.ts
+++ src/routes/jeuRouter.ts
@@ -50,7 +50,6 @@ export class JeuRouter {
       this._errorCode500(error, req, res);
     }
   }
-
   /**
    * jouer une fois aux dés
    */
@@ -106,6 +105,26 @@ export class JeuRouter {
     }
   }
 
+   /**
+   * redémarrer le jeu
+   */
+  public redemarrerJeu(req: Request, res: Response, next: NextFunction) {
+   
+    try{
+       req.flash('info', `Redémarrage du jeu en cours...`);
+      this._controleurJeu.redemarrerJeu();
+     
+       res.status(200)
+        .send({
+          message: 'Success',
+          status: res.status,
+        });
+    }catch(error){
+      this._errorCode500(error,req,res)
+    }
+  }
+
+
   /**
      * Take each handler, and attach to one of the Express.Router's
      * endpoints.
@@ -114,6 +133,7 @@ export class JeuRouter {
     this._router.post('/demarrerJeu', this.demarrerJeu.bind(this)); // pour .bind voir https://stackoverflow.com/a/15605064/1168342
     this._router.get('/jouer/:nom', this.jouer.bind(this)); // pour .bind voir https://stackoverflow.com/a/15605064/1168342
     this._router.get('/terminerJeu/:nom', this.terminerJeu.bind(this)); // pour .bind voir https://stackoverflow.com/a/15605064/1168342
+    this._router.get('/redemarrerJeu', this.redemarrerJeu.bind(this)); // pour .bind voir https://stackoverflow.com/a/15605064/1168342
   }
 
 }
